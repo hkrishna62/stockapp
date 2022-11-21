08d8b553f4a6ec7eb6368221dba2c2b57932c233
hkrishna62 <hkrishna62.62@gmail.com>
2022-11-21 14:56:15 +0530

    changes done

--- src/main/java/com/mergsoft/stockapp/controller/AdminController.java
+++ src/main/java/com/mergsoft/stockapp/controller/AdminController.java
@@ -1,10 +1,9 @@
 package com.mergsoft.stockapp.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.mergsoft.stockapp.entity.User;
 import com.mergsoft.stockapp.service.AdminService;
@@ -15,10 +14,10 @@ public class AdminController {
 	// @Autowired
 	private AdminService adminService;
 	
-	@PostMapping("/createuser")
-	public String createUser(@ModelAttribute("user") User user, Model model) {
+	@GetMapping("/getcreateuserform")
+	public String getcreateUserForm(@ModelAttribute("user") User user, Model model) {
 		
-		System.out.println("");
+		System.out.println(""+user.getUserId());
 		return "admin";
 		
 	}
